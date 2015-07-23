7964d09aa2ab8376589746d800efe3ad3b244eac
Robin Jeffries <rjeffries@csuchico.edu>
2015-07-23 09:17:44 -0700

    Revert "bug fix to sequences and subsetting"
    This reverts commit d7b83fd47feb14baa2390ce7d67a91a12d667b76.

--- create swirl lessons.R
+++ create swirl lessons.R
@@ -5,10 +5,8 @@ library(swirlify)
 new_lesson("Subsetting Vectors", "R Bootcamp")
 
 # Open existing lesson
-set_lesson("C:/GitHub/R Bootcamp/R_Bootcamp/Subsetting_Vectors/lesson.yaml")
+set_lesson("C:/Dropbox/CSUC/RBootcamp/R_Bootcamp/Basic_Building_Blocks/lesson.yaml")
 
-# Test the lesson
-testit()
 
 # How to install the course from a zip file
 install_course_zip("path/to/file/here/swirl_courses-master.zip", multi=TRUE, 
